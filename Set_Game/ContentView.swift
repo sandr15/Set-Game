98ab0bcd12a40149d006dd671d5abad8eec7b414
Andrew Siemon <sandr15@vt.edu>
2020-11-27 14:10:39 -0800

    Initial Commit

---
+++ Set_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set_Game
+//
+//  Created by Andrew on 11/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
